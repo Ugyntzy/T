5b588c60b0e1c045b24fb084a1c20874c0dd6857
unknown <02230092.cst@rub.edu.bt>
2024-03-01 16:57:55 +0600

    logical operator

---
+++ logical.py
@@ -0,0 +1,17 @@
+X=5
+print(X>3 and X<10)
+
+y=12
+print(y>5 and y%5==0)
+
+X=5
+print(X>3 or X<10)
+
+y=12
+print(y>5 or y%5==0)
+
+X=5
+print(not(X>3 and X<10))
+
+y=12
+print(not(y>5 and y%5==0))
