ba6ae70f08110a825cccdcbf5f8b54bcd7fab12c
unknown <02230092.cst@rub.edu.bt>
2024-03-01 16:49:33 +0600

    class activity

---
+++ class activity.py
@@ -0,0 +1,19 @@
+
+name = input("MING LAPSH ALO: ")
+
+
+num1 = float(input("YA NUMBER CHI LAPSH: "))
+num2 = float(input("DORU NUMBER CHI: "))
+
+
+add_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+div_result = num1 / num2
+
+
+print(f"YET ALO , {name}!")
+print(f"Sum of {num1} and {num2} is: {add_result:.2f}")
+print(f"Difference between {num1} and {num2} is: {sub_result:.2f}")
+print(f"Product of {num1} and {num2} is: {mul_result:.2f}")
+print(f"Division of {num1} by {num2} is: {div_result:.2f}")
